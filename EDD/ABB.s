f38f1683fb90045489028e3d9c5ee9a8814a18dc
Jorge Castañeda <3030878110108@ingenieria.usac.edu.gt>
2025-02-04 15:47:07 -0600

    Insercion y Recorrido En Orden Del Arbol Binario

--- EDD/ABB.s
+++ EDD/ABB.s
@@ -29,8 +29,7 @@
 
 .section .bss
     .align 2
-    opcion:         .space 4, 0
-    dummy:          .space 4, 0
+    opcion:         .space 8, 0
     numero:         .space 4, 0
 
     .align 3
@@ -38,10 +37,9 @@
     stack:          .xword 0
 
 .section .text
-.global main
-.global insert_routine
-.global print_tree
 
+    .type insert_routine, %function
+    .global insert_routine
 insert_routine:
     STP x29, x30, [SP, #-16]!
 
@@ -52,12 +50,14 @@ insert_routine:
     BL malloc                   // Funcion para reservar bytes
     CBZ x0, malloc_handler_err  // Captura del error
 
-    LDR x1, =root               // Direccion de memoria donde se almacena el root
-    LDR x1, [x1]                // Cargar Direccion de memoria reservada para root
+    verificar_root:
+        LDR x1, =root               // Direccion de memoria donde se almacena el root
+        LDR x3, [x1]                // Cargar Direccion de memoria reservada para root
 
-    CBNZ x1, root_not_null      // Si el root == NULL, que inserte el primer nodo en root
-
-    LDR x1, =root           // Cargar la direccion de root
+        CBZ x3, save_node           // Si el root == NULL, que inserte el primer nodo en root
+        LDR x1, [x1]                // Caso contrario, cargar el valor de la memoria almacenada en root
+        LDR x3, [x1]                // Cargar el valor del nodo root en x3
+        B root_not_null             // Hacer el salto a "root_not_null"
 
     save_node:
         STR x0, [x1]            // almacenar la direccion del nodo en root
@@ -68,41 +68,40 @@ insert_routine:
         ADR x0, insert_success  // msg de aviso que todo salio bien
         BL printf               // Funcion para imprimir en pantalla
 
-        LDP x29, x30, [SP], #16
+        LDP x29, x30, [SP], #16     
         RET
 
     root_not_null:
         // Aplicar recursividad en forma de un ciclo
-        LDR x2, [x1]    // Cargar el valor del nodo en x2
-        // hacer ciclo para recorrer los nodos
-
-        CMP x19, x2
-        BLE child_izq   // Si el valor es menor o igual al nodo, que se vaya al lado izquierdo
-        B child_rig     // Si el valor es mayor, que se vaya al lado derecho
+        CMP x19, x3
+        BLE child_izq                   // Si el valor es menor o igual al nodo, que se vaya al lado izquierdo
+        B child_rig                     // Si el valor es mayor, que se vaya al lado derecho
 
         child_izq:
-            LDR x2, [x1, #8]            // Cargar el puntero izquierdo
-            ADD x1, x1, #8              // Sumarle 8 bytes al offset
-
-            CBNZ x2, root_not_null      // Si el nodo no esta vacio, regresar al ciclo para seguir bajando
-            B save_node                 // Si el nodo esta vacio, guardar en la direccion con el offset aplicado
+            LDR x2, [x1, #8]!            // Cargar el puntero izquierdo
+            CBZ x2, save_node            // Si el valor del ptr izq es null, que guarde en ese espacio de memoria el nuevo nodo
+            MOV x1, x2                   // Caso contrario, que el valor del nodo lo copie en x1
+            LDR x3, [x2]                 // Cargar el valor del nodo en el registro x3
+            B root_not_null              // Hacer el salto hacia el "root_not_null"
 
         child_rig:
-            LDR x2, [x1, #16]           // Cargar el puntero derecho
-            ADD x1, x1, #16             // Sumarle 16 bytes al offset
-
-            CBNZ x2, root_not_null      // Si el nodo no esta vacio, regresar al ciclo para seguir bajando
-            B save_node                 // Si el nodo esta vacio, guardar en la direccion con el offset aplicado
+            LDR x2, [x1, #16]!              // Cargar el puntero derecho
+            CBZ x2, save_node               // Si el valor del ptr der es null, que guarde en ese espacio de memoria el nuevo nodo
+            MOV x1, x2                      // Caso contrario, que el valor del nodo lo copie en x1
+            LDR x3, [x2]                    // Cargar el valor del nodo en el registro x3
+            B root_not_null                 // Hacer el salto hacia el "root_not_null"
         
     malloc_handler_err:
-        LDR x0, =malloc_err             // Notificar un error, si no se pudo reservar memoria en el heap
-        BL printf
+        LDR x0, =malloc_err                 // Notificar un error, si no se pudo reservar memoria en el heap
+        BL printf                           
 
     LDP x29, x30, [SP], #16
     RET
+    .size insert_routine, (. - insert_routine)
 
 
-// ! CORREGIR RECORRIDO DEL ARBOL EN ESTE PROCEDIMIENTO
+    .type print_tree, %function
+    .global print_tree
 print_tree:
     STP x29, x30, [SP, #-16]!
 
@@ -110,66 +109,58 @@ print_tree:
     MOV x11, SP             // Copiar el valor del puntero stack en el registro x11
     STR x11, [x0]           // Almacenar el puntero inicial de la pila para restaurarla hasta este punto
 
-    ADR x0, msg_head_print
-    BL printf
+    ADR x0, msg_head_print      // Imprimir encabezado para la lista de nodos
+    BL printf                   
 
     LDR x0, =root       // Cargar la direccion de memoria de la variable root
     LDR x0, [x0]        // Cargar el puntero del primer nodo del arbol      
-    MOV x10, #0         // Inicializar bandera para el ajuste de la pila
 
     // Recorrido inorden
     recorrido_izq:
-        LDR x1, [x0, #8]
-        CBZ x1, print_node
-        STR x0, [SP, #-8]!
-        MOV x0, x1
-        B recorrido_izq
+        LDR x1, [x0, #8]                // Cargar la direccion almacenada en el ptr izquierdo
+        CBZ x1, print_node              // Si el valor del ptr izq es null, que imprima salte a imprimir el nodo actual
+        MOV x20, #0                     // Relleno De Pila
+        STP x0, x20, [SP, #-16]!        // Almacenar en la pila la direccion del nodo actiual Si el valor del ptr izq es diferente de null
+        MOV x0, x1                      // Copiar el valor del ptr izq en el registro x0
+        B recorrido_izq                 // Saltar a "recorrido_izq"
 
     recorrido_der:
-        LDR x1, [x0, #16]
-        CBZ x1, print_node
+        LDR x1, [x0, #16]               // Cargar la direccion almacenada en el ptr derecho
+        CBZ x1, back_father             // En caso de que el puntero derecho sea null, saltar a "back_father"
+        MOV x0, x1                      // Copiar la direccion del nodo de x1 a x0
+        B recorrido_izq                 // Saltar a "recorrido_izq"
+        
+    back_father:
+        LDR x1, =stack                  // Cargar la direccion de memoria de la variable stack
+        LDR x1, [x1]                    // Cargar el valor inicial del stack almacenado
+        CMP SP, x1                      // Si el apuntador de pila es igual al valor guardado, ya no hay nodos para sacar
+        BEQ exit_print_tree             // Terminar funcion
 
-        LDR x1, =stack
-        LDR x1, [x1]
-        CMP SP, x1
-        BEQ exit_print_tree
+        LDP x0, x20, [SP], #16          // Cargar la direccion del nodo padre en x0, desde la pila, x20 es padding
 
-        LDR x0, [SP], #8
-        MOV x1, x2
-        B recorrido_izq
-
-    // Validar alineacion de la pila para no cometer errores
     print_node:
-        MOV x9, SP
-        AND x9, x9, #0xF
-        CBZ x9, aligned
-        SUB SP, SP, #8
-        MOV x10, #1
+        MOV x15, x0                 // Copiar la direccion de memoria del nodo en el registro x15, para no perder la referencia
 
-        aligned:
-            // Imprimir el nodo en la terminal
-            LDR x1, [x0]
-            LDR x0, =format_node
-            LDR x2, [x0, #8]
-            LDR x3, [x0, #16]
-            BL printf
+        // Configurando parametros para la impresion del nodo en la consol
+        LDR x1, [x0]                // Cargar el valor del nodo en x1
+        LDR x2, [x0, #8]            // Cargar el valor de memoria del ptr izq
+        LDR x3, [x0, #16]           // Cargar el valor de memoria del ptr der
+        LDR x0, =format_node        // Cargar la direccion del formato a imprimir por terminal
+        BL printf
 
-            CBNZ x10, adj_sp
-            B no_adj_sp
-
-        adj_sp:
-            ADD SP, SP, #8
-            MOV x10, #0
-
-        no_adj_sp:
-            B recorrido_der
+        MOV x0, x15                 // Recuperar el valor de la memoria de x15 a x0
+        B recorrido_der             // Hacer el salto a "recorrido_der"
 
     exit_print_tree:
         LDP x29, x30, [SP], #16
         RET
 
+    .size print_tree, (. - print_tree)
+
 
 // Estructura Del Nodo: | 4 bytes (num) | 4 bytes (padding) | 8 bytes (puntero izquierdo) | 8 bytes (puntero derecho) | => 24 bytes
+    .type main, %function
+    .global main
 main:
     LDR x0, =menu
     BL printf
@@ -218,7 +209,7 @@ main:
         BL insert_routine
 
         LDR x0, =opcion
-        STR wzr, [x0]
+        STR xzr, [x0]
 
         B main
 
@@ -230,10 +221,12 @@ main:
 
     print_routine_op:
         LDR x0, =opcion
-        STR wzr, [x0]
+        STR xzr, [x0]
 
         BL print_tree
         B main
 
     exit_program:
         BL exit
+
+    .size main, (. - main)
